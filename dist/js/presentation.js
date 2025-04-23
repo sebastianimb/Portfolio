b0aef238ae069d102ace61762c6a1ce357faa093
sebastian molina <cirp8918@gmail.com>
2025-04-22 21:22:13 -0400

    Cambio de dise;o

--- dist/js/presentation.js
+++ dist/js/presentation.js
@@ -3,6 +3,30 @@ const titleBody = document.querySelector(".container-data-body .title h1");
 const resumeBody = document.querySelector(".container-data-body .body-resume");
 const studyBody = document.querySelector(".container-data-body .body-study");
 const HobbyBody = document.querySelector(".container-data-body .body-hobby");
+const linkEstudios = document.querySelector("#LinkEstudios");
+const linkPasatiempo = document.querySelector("#LinkPasatiempo");
+linkEstudios.addEventListener("click", function () {
+  titleBody.textContent = "Estudios";
+  resumeBody.style.display = "none";
+  studyBody.style.display = "flex";
+  HobbyBody.style.display = "none";
+  this.classList.add("active");
+  const estudiosItem = document.querySelector("#aside-data li#estudios");
+  if (estudiosItem) {
+    estudiosItem.click();
+  }
+});
+linkPasatiempo.addEventListener("click", function () {
+  titleBody.textContent = "Pasatiempos";
+  resumeBody.style.display = "none";
+  studyBody.style.display = "flex";
+  HobbyBody.style.display = "none";
+  this.classList.add("active");
+  const PaatiemposItem = document.querySelector("#aside-data li#pasatiempos");
+  if (PaatiemposItem) {
+    PaatiemposItem.click();
+  }
+});
 liData.forEach((item, index) => {
   item.addEventListener("click", function () {
     liData.forEach((i) => {
